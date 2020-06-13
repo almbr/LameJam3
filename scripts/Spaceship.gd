60099415f6d7366e742348df41174d9240c5337a
Erik Almbratt <erik.almbratt@gmail.com>
2020-06-13 02:15:58 +0200

    Massa ful kod....

--- scripts/Spaceship.gd
+++ scripts/Spaceship.gd
@@ -1,20 +1,69 @@
-extends Spatial
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+extends RigidBody
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
+	
+func _physics_process(delta):
+	thrust = Vector3()
+	torque = Vector3()
+	parse_input(delta)
+	add_central_force(thrust)
+	#add_central_force_local(thrust)
+	add_torque(torque)
+	
+	# Parent follow child
+	#set_translation(hull.get_global_transform().origin)
 
-func _input(event):
-	print(event.as_text())
+export var side_thrusters_torqe = Vector3(0,100,0)
+export var forward_thrust = 100
+export var reverse_thrust = -50
+export var side_thruster_impulse = 40
 
+var thrust = Vector3()
+var torque = Vector3()
+	
+func thruster(thruster):
+	# Thrust
+	if (thruster == "forward") && (thruster == "reverse"):
+		thrust = Vector3()
+	elif thruster == "forward":
+		thrust = transform.basis.z * forward_thrust
+		#translate_object_local(-transform.basis.z)
+	elif thruster == "reverse":
+		thrust = transform.basis.z * reverse_thrust
+		#ranslate_object_local(transform.basis.z)
+		
+	# Rotation
+	if (thruster == "left") && (thruster == "right"):
+		torque = Vector3()
+	elif thruster == "left":
+		torque = side_thrusters_torqe
+	elif thruster == "right": 
+		torque = side_thrusters_torqe * Vector3(1,-1,1)
+
+func parse_input(delta):
+	if Input.is_action_pressed("Fire"):
+		print("fire")
+		pass #fire()
+	if Input.is_action_pressed("ui_up"):
+		thruster("forward")
+	elif Input.is_action_pressed("ui_down"):
+		thruster("reverse")
+	if Input.is_action_pressed("ui_left"):
+		#hull.rotate_object_local(Vector3(0, 1, 0), delta)
+		thruster("left")
+	if Input.is_action_pressed("ui_right"):
+		#hull.rotate_object_local(Vector3(0, -1, 0), delta)
+		thruster("right")
+
+#func _input(event):
+#	if event.is_action_pressed("Left_side"):
+#		hull.apply_central_impulse(Vector3(side_thruster_impulse,1,1)*hull.transform.basis.x)
+#	if event.is_action_pressed("Right_side"):
+#		hull.apply_central_impulse(Vector3(-side_thruster_impulse,1,1))
