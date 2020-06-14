8e35c149ac17e00f0e581e8b6bda21d20a9293dd
Erik Almbratt <erik.almbratt@gmail.com>
2020-06-14 13:22:22 +0200

    Nätverk fungerar!

--- scripts/Spaceship.gd
+++ scripts/Spaceship.gd
@@ -1,6 +1,15 @@
 extends RigidBody
 
-# Called when the node enters the scene tree for the first time.
+var is_master = false
+
+func initialize(id):
+	name = str(id)
+	if id == Net.net_id:
+		is_master = true
+	#else:
+	#	modulate = Color8(255, 0, 0, 255)
+	
+
 func _ready():
 	pass
 
@@ -10,20 +19,21 @@ func _process(delta):
 	pass
 	
 func _physics_process(delta):
-	thrust = Vector3()
-	torque = Vector3()
-	parse_input(delta)
-	add_central_force(thrust)
-	#add_central_force_local(thrust)
-	add_torque(torque)
-	
-	# Parent follow child
-	#set_translation(hull.get_global_transform().origin)
+	if is_master:
+		thrust = Vector3()
+		torque = Vector3()
+		parse_input(delta)
+		add_central_force(thrust)
+		add_torque(torque)
+		rpc_unreliable("update_translation", translation)
+		
+remote func update_translation(tran):
+	translation = tran
 
 export var side_thrusters_torqe = Vector3(0,100,0)
 export var forward_thrust = 100
 export var reverse_thrust = -50
-export var side_thruster_impulse = 40
+export var side_thruster_impulse = 60
 
 var thrust = Vector3()
 var torque = Vector3()
@@ -34,10 +44,8 @@ func thruster(thruster):
 		thrust = Vector3()
 	elif thruster == "forward":
 		thrust = transform.basis.z * forward_thrust
-		#translate_object_local(-transform.basis.z)
 	elif thruster == "reverse":
 		thrust = transform.basis.z * reverse_thrust
-		#ranslate_object_local(transform.basis.z)
 		
 	# Rotation
 	if (thruster == "left") && (thruster == "right"):
@@ -56,14 +64,12 @@ func parse_input(delta):
 	elif Input.is_action_pressed("ui_down"):
 		thruster("reverse")
 	if Input.is_action_pressed("ui_left"):
-		#hull.rotate_object_local(Vector3(0, 1, 0), delta)
 		thruster("left")
 	if Input.is_action_pressed("ui_right"):
-		#hull.rotate_object_local(Vector3(0, -1, 0), delta)
 		thruster("right")
 
-#func _input(event):
-#	if event.is_action_pressed("Left_side"):
-#		hull.apply_central_impulse(Vector3(side_thruster_impulse,1,1)*hull.transform.basis.x)
-#	if event.is_action_pressed("Right_side"):
-#		hull.apply_central_impulse(Vector3(-side_thruster_impulse,1,1))
+func _input(event):
+	if event.is_action_pressed("Left_side"):
+		apply_central_impulse(transform.basis.x*side_thruster_impulse)
+	if event.is_action_pressed("Right_side"):
+		apply_central_impulse(-transform.basis.x*side_thruster_impulse)
